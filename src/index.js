b10dede6b94077a407f29841855df150e8489aa2
JunMango <gudwns4985@gmail.com>
2024-10-18 23:41:19 +0900

    update:react18 버전에서 creatRoot 방식을 지향한다고 해서 적용해봤다. https://react.dev/reference/react-dom/client/createRoot

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// serviceWorkerRegistration.register();
+
 reportWebVitals();
