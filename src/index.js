086bc43c10f649554d39f355f47ce5fbbd85ba16
JunMango <gudwns4985@gmail.com>
2024-11-04 15:16:01 +0900

    fix:tailwind 설정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import 'tw-elements-react/dist/css/tw-elements-react.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements-react/dist/js/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("tw-elements-react/dist/plugin.cjs")],
 };
